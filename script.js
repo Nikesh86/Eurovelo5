12e9197b466789aaeb8648c6da25a15ac73f1823
Nikesh Shrestha <shresthanik@gmail.com>
2023-03-06 15:06:35 +0100

    map

---
+++ script.js
@@ -0,0 +1,25 @@
+var map = L.map('map').setView([50.6887186, 3.1667413], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var gpx = '/gpx/gpx.xml'; // URL to your GPX file or the GPX itself
+new L.GPX(gpx, {
+  async: true,
+  marker_options: {
+    startIconUrl: '/image/pin-icon-start.png',
+    endIconUrl: '/images/pin-icon-end.png',
+    shadowUrl: '/images/pin-shadow.png',
+  }
+
+}).on('loaded', function (e) {
+  map.fitBounds(e.target.getBounds());
+}).addTo(map);
+
+function onMapClick(e) {
+  alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
